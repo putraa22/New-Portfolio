6ab32affae0c395bb934549545cb4b04250adde6
Hermansyah Putra <hermansyahputra0418@gmail.com>
2023-09-07 13:49:12 +0700

    Home Menu

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,20 @@
+import React from "react";
+
+// next image
+import Image from "next/image";
+
+// next link
+import Link from "next/link";
+
+// components
+import Socials from "./Socials";
+
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px] ">
+      <div className="container mx-auto">Header</div>
+    </header>
+  );
 };
 
 export default Header;
--- components/Socials.js
+++ components/Socials.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const Socials = () => {
   return <div>Socials</div>;
 };
