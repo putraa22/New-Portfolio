994fe272bed74ede3c1f0d61b9ab536293480215
Hermansyah Putra <hermansyahputra0418@gmail.com>
2023-09-09 12:54:32 +0700

    About

--- components/Header.js
+++ components/Header.js
@@ -12,7 +12,22 @@ import Socials from "./Socials";
 const Header = () => {
   return (
     <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px] ">
-      <div className="container mx-auto">Header</div>
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row justify-between items-center gap-y-2 py-6">
+          {/* Logo */}
+          <Link href={"/"}>
+            <Image
+              src={"/logo.svg"}
+              alt="logo-image"
+              width={280}
+              height={48}
+              priority={true}
+            />
+          </Link>
+          {/* Social Link */}
+          <Socials />
+        </div>
+      </div>
     </header>
   );
 };
--- components/Socials.js
+++ components/Socials.js
@@ -1,7 +1,25 @@
 import React from "react";
 
+// next link
+import Link from "next/link";
+
+// icons
+import { RiInstagramLine, RiFacebookLine, RiGithubLine } from "react-icons/ri";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiInstagramLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiFacebookLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transition-all duration-300">
+        <RiGithubLine />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
