760daa6ced1d96701b3835a4e4764903e171e838
Hermansyah Putra <hermansyahputra0418@gmail.com>
2023-09-10 15:25:52 +0700

    Finish Service section

--- components/Bulb.js
+++ components/Bulb.js
@@ -1,5 +1,20 @@
+import React from "react";
+
+// next image
+import Image from "next/image";
+
 const Bulb = () => {
-  return <div>Bulb</div>;
+  return (
+    <div className="absolute -left-32 -bottom-12 rotate-12 mix-blend-color-dodge animate-pulse duration-75 z-10 w-[200px] xl:w-[260px]">
+      <Image
+        src={"/bulb.png"}
+        width={260}
+        height={200}
+        className="w-full h-full"
+        alt="bulb-images"
+      />
+    </div>
+  );
 };
 
 export default Bulb;
--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -1,3 +1,13 @@
+import React from "react";
+
+// import swiper react component
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import swiper style
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
 // icons
 import {
   RxCrop,
@@ -5,40 +15,89 @@ import {
   RxDesktop,
   RxReader,
   RxRocket,
+  RxArrowTopRight,
 } from "react-icons/rx";
 
-
 // data
-const serviceData = [
-  {
-    icon: <RxCrop />,
-    title: 'Branding',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
+export const serviceData = [
   {
     icon: <RxPencil2 />,
-    title: 'Design',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "UI Development",
+    description:
+      "I will design and develop user interfaces that are engaging and focused on optimal user experience. ",
+  },
+  {
+    icon: <RxCrop />,
+    title: "Responsive",
+    description:
+      "The websites and apps I create will be responsive, will adjust to different device screen sizes.",
   },
   {
     icon: <RxDesktop />,
-    title: 'Development',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
-  {
-    icon: <RxReader />,
-    title: 'Copywriting',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "Development",
+    description:
+      "I will implement the necessary interactions and functions using JavaScript in the development process.",
   },
   {
     icon: <RxRocket />,
-    title: 'SEO',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "Performance",
+    description:
+      "I will make sure your website or app has fast load times and optimized performance.",
+  },
+  {
+    icon: <RxReader />,
+    title: "API Integration",
+    description:
+      "If you require integration with third-party services or APIs, I can manage this for you.",
   },
 ];
 
+// import required module
+import { FreeMode, Pagination } from "swiper";
+
 const ServiceSlider = () => {
-  return <div>Service Slider</div>;
+  return (
+    <Swiper
+      breakpoints={{
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 15,
+        },
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 15,
+        },
+      }}
+      freeMode={true}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[FreeMode, Pagination]}
+      className="h-[240px] sm:h-[340px] "
+    >
+      {serviceData.map((item, index) => {
+        return (
+          <SwiperSlide key={index}>
+            <div className=" bg-indigo-700/5 h-max rounded-lg px-6 py-5 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-white/5 transition-all duration-300">
+              {/* icon */}
+              <div className="text-4xl text-accent mb-2 ">{item.icon}</div>
+              {/* title & Desc */}
+              <div className="mb-5">
+                <div className="mb-1 text-lg ">{item.title}</div>
+                <p className="max-w-[350px] leading-normal ">
+                  {item.description}
+                </p>
+              </div>
+              {/* Arrow */}
+              <div className="text-3xl ">
+                <RxArrowTopRight className="group-hover:rotate-45 group-hover:text-accent transition-all duration-300" />
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
 export default ServiceSlider;
